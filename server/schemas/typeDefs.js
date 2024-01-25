200acc95df9356a244128be05e98fd79c76ab34c
toum21 <pey2moo1@gmail.com>
2024-01-25 13:49:15 -0800

    building out typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,6 +1,37 @@
 const gql = String
 
 const typeDefs = gql`
+type Book {
+    _id: ID!
+    authors: String
+    description: String!
+    bookId: String!
+    image: String!
+    link: String
+    title: String!
+}
+
+type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    savedBooks: [Book]
+}
+
+type Auth {
+    token: ID!
+    user: User
+  }
+
+type Query {
+    me: User
+}
+
+type Mutation {
+    login(email: String!, password: String!): Auth
+    
+}
 
 `
 
