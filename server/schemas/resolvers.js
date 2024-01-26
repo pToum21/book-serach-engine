aeba54a8a49120176de0e62bd2289fe64b08bf95
toum21 <pey2moo1@gmail.com>
2024-01-26 12:56:55 -0800

    working on graphql error

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,8 +3,13 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        me: async () => {
-            return User.findOne({ username })
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id }).select('_v -password');
+
+                return userData
+            }
+            throw AuthenticationError;
         }
     },
 
