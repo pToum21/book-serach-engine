e4da34b579046e645b58f4ef0b3c8a81dedba919
toum21 <pey2moo1@gmail.com>
2024-01-26 16:21:40 -0800

    working inside of signupFrom

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
+import { ADD_USER } from '../utils/mutations';
+import { useMutation } from '@apollo/client';
+
 
 const SignupForm = () => {
   // set initial form state
@@ -28,7 +30,7 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const { data } = await 
 
       if (!response.ok) {
         throw new Error('something went wrong!');
--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -14,10 +14,10 @@ import { removeBookId } from '../utils/localStorage';
 import Auth from '../utils/auth';
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  const { loading, data } = useQuery(QUERY_ME);
+  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
 
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+  const userData = data?.me || {};
 
 
   // create function that accepts the book's mongo _id value as param and deletes the book from the database
@@ -29,23 +29,19 @@ const SavedBooks = () => {
     }
 
     try {
-      const response = await deleteBook(bookId, token);
+      const { data } = await removeBook({
+        variables: {bookId}
+      })
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
+      removeBookId(bookId)
 
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
-      removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
   };
 
   // if data isn't here yet, say so
-  if (!userDataLength) {
+  if (loading) {
     return <h2>LOADING...</h2>;
   }
 
